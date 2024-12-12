29cbd3ef64d32014e455245edc7d5456155b98f6
Prasad-R-Patil <prasadpatil24212@gmail.com>
2024-12-12 14:08:50 +0530

    cmt

--- ProblemOnString/IOstring.cpp
+++ ProblemOnString/IOstring.cpp
@@ -5,7 +5,14 @@ using namespace std;
 
 int main()
 {
+   char ch = '\0';
+   char str[50];
+
+   cout<<"Enter your string :- ";
+   cin.getline(str,30);
+
+   cout<<"You entered String is :- "<<str<<endl;
  
- 
+ return 0;
 
 }
